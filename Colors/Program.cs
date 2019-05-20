8f3eb8a78374db07c5084c06ce5a40feb73a3564
NickWolfen99 <strftwolfen99@gmail.com>
2019-05-20 22:07:16 +0300

    Initial commit

--- Colors/Program.cs
+++ Colors/Program.cs
@@ -10,6 +10,61 @@ namespace Colors
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter number of colors:");
+            int n = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter: 1 for red; 2 for white; 3 for blue.  ");
+
+            string[] colorsarray = new string[n];
+
+            string[] colors = { "red","blue","white" };
+
+            for (int i = 0; i < n; i++)
+            {
+                int index = Convert.ToInt16(Console.ReadLine());
+                colorsarray[i] = colors[index-1];
+            }
+
+            Console.WriteLine("Your colors are:");
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(colorsarray[i]);
+
+            int counter = 0;
+            string swap;
+            for (int i = 0; i < n; i++)
+            {
+                if (colorsarray[i] == "red")
+                {
+                    if(counter!=i)
+                    {
+                        swap = colorsarray[counter];
+                        colorsarray[counter] = colorsarray[i];
+                        colorsarray[i]=swap;
+                    }
+                    counter++;
+
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (colorsarray[i] == "blue")
+                {
+                    if (counter != i)
+                    {
+                        swap = colorsarray[counter];
+                        colorsarray[counter] = colorsarray[i];
+                        colorsarray[i] = swap;
+                    }
+                    counter++;
+
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Your arranged colors are:");
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(colorsarray[i]);
+
+            Console.ReadLine();
         }
     }
 }
